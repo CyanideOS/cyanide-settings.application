e89d2721ae9ad1e5f59bb191ab9b7b29ad356335
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-09-25 17:23:16 +0530

    Fixed some thing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { NativeSdkModule } from './components/native-sdk/native-sdk.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './routes/home/home.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule, NativeSdkModule],
+  imports: [BrowserModule, AppRoutingModule, NativeSdkModule, DragDropModule],
   providers: [],
   bootstrap: [AppComponent],
 })
